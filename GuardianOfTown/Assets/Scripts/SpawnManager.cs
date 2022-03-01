5b1c277fa91145861b579616420466af725eaf9a
Meguipascual <meguipascual@gmail.com>
2022-03-01 21:09:28 +0100

    Spawn Randomized

--- GuardianOfTown/Assets/Scripts/SpawnManager.cs
+++ GuardianOfTown/Assets/Scripts/SpawnManager.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject [] enemyPrefab;
-    [SerializeField] private float spawnSpeed;
-    private int wavesCleared;
+    private float spawnSpeed = 3.0f;
+    private int actualWave = 6;
 
     // Start is called before the first frame update
     void Start()
     {
-        spawnSpeed = 3.0f;
-        wavesCleared = 1;
-        InvokeRepeating("SpawnEnemies", 3.0f, spawnSpeed / wavesCleared);
+        
+        InvokeRepeating("SpawnEnemies", 3.0f, spawnSpeed / actualWave);
     }
 
     private void SpawnEnemies()
     {
         var enemyType = Random.RandomRange(0, enemyPrefab.Length);
+        var enemyX = Random.RandomRange(-23, 23);
+        var enemyY = 1;
+        var enemyZ = 20f;
+        Vector3 enemyPosition = new Vector3(enemyX, enemyY, enemyZ);
 
-        Instantiate(enemyPrefab[enemyType]);
+        Instantiate(enemyPrefab[enemyType], enemyPosition, gameObject.transform.rotation);
     }
 }
