7d4d4d3c6b44b3c5fae8e3c8c7ea1a6f904287a9
Meguipascual <meguipascual@gmail.com>
2023-08-22 19:41:00 +0200

    Boss female script created in order to differenciate between bosses and troll animation created

---
+++ GuardianOfTown/Assets/Scripts/Enemies/FemaleBossManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
+public class FemaleBossManager : Enemy
+{
+    [SerializeField] private GameObject floatingTextPrefab;
+    [SerializeField] private GameObject criticalHitTextPrefab;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        Level = SpawnManager.LevelOfBosses;
+        Attack = 100;
+        HP = 500;
+        HpMax = HP;
+        Defense = 10;
+        Speed = 0.5f;
+        Exp = 50;
+        TimeToMove = 2f;
+        TimeToRest = 2f;
+        EnemyMove = "EnemyMove";
+        LevelUp();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player.IsDead) return;
+
+        TryToMove();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Trigger(other, floatingTextPrefab, criticalHitTextPrefab);
+    }   
+}
