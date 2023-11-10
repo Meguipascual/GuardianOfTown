0866a22d0ba7c301afa1df924e99e6a3b989d10f
Meguipascual <meguipascual@gmail.com>
2023-11-10 14:01:49 +0100

    StepsSounds and 3d sound adjustements

--- GuardianOfTown/Assets/Scripts/Enemies/Enemy.cs
+++ GuardianOfTown/Assets/Scripts/Enemies/Enemy.cs
@@ -118,6 +118,7 @@ public abstract class Enemy : Character
         }
         else
         {
+            _soundManager.PlayRandomStepSound();
             IsResting = true;
             MovementTimer = TimeToMove;
         }
